7b44ded921fadc56b4f5cf153e8a01a438e0cfc8
Jude Beaton <jude.a.beaton@gmail.com>
2022-11-01 15:36:00 +0000

    First template for portfolio

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
 import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import deved from "../public/dev-ed-wave.png";
+import design from "../public/design.png";
+import code from "../public/code.png";
+import consulting from "../public/consulting.png";
+import web1 from "../public/web1.png";
+import web2 from "../public/web2.png";
+import web3 from "../public/web3.png";
+import web4 from "../public/web4.png";
+import web5 from "../public/web5.png";
+import { useState } from "react";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <div>
+    <div div className={darkMode ? "dark" : ""}>
       <Head>
         <title>Jude Beaton Portfolio</title>
         <meta
@@ -14,9 +31,157 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <section className=" bg-blue-700 min-h-screen">
-          <h1>Hello</h1>
+      <main className="bg-white px-10 md:px-20 lg:px-50 dark:bg-gray-900">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-burtons">Jude Beaton</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className=" cursor-pointer text-2xl"
+                />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
+              Jude Beaton
+            </h2>
+            <h3 className="text-2xl py-2 md:text-3xl">
+              Computer Science undergrad.
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto">
+              Looking for work experience before a Bachelors in Computer
+              Science, take a look at my projects, links to GitHub and YouTube
+              are below.
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillYoutube />
+          </div>
+          <div className="overflow-hidden relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 md:h-96 md:w-96">
+            <Image src={deved} fill object-fit="cover" alt="" />
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className=" text-3xl py-1">Skills I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Some text about my <span className="text-teal-500">skills</span>{" "}
+              and <span className="text-teal-500">knowledge</span> in computer
+              science
+            </p>
+          </div>
+          <div className="lg:flex gap-10 justify-center">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 ">
+              <div className="flex justify-center">
+                <Image src={design} width={100} height={100} alt="" />
+              </div>
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                A-Level Coursework
+              </h3>
+              <p className="py-2"> Summary of my NEA</p>
+              <h4 className="py-4 text-teal-600">Technology used</h4>
+              <p className="text-gray-800 py-1">P5.js</p>
+              <p className="text-gray-800 py-1">Python</p>
+              <p className="text-gray-800 py-1">Selenium</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 ">
+              <div className="flex justify-center">
+                <Image src={code} width={100} height={100} alt="" />
+              </div>
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                A-Level Coursework
+              </h3>
+              <p className="py-2"> Summary of my NEA</p>
+              <h4 className="py-4 text-teal-600">Technology used</h4>
+              <p className="text-gray-800 py-1">P5.js</p>
+              <p className="text-gray-800 py-1">Python</p>
+              <p className="text-gray-800 py-1">Selenium</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 ">
+              <div className="flex justify-center">
+                <Image src={consulting} width={100} height={100} alt="" />
+              </div>
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                A-Level Coursework
+              </h3>
+              <p className="py-2"> Summary of my NEA</p>
+              <h4 className="py-4 text-teal-600">Technology used</h4>
+              <p className="text-gray-800 py-1">P5.js</p>
+              <p className="text-gray-800 py-1">Python</p>
+              <p className="text-gray-800 py-1">Selenium</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Portfolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Some text about my <span className="text-teal-500">projects</span>{" "}
+              and <span className="text-teal-500">experience</span> in computer
+              science
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-tow lg:flex-wrap">
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web1}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                alt=""
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web2}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                alt=""
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web3}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                alt=""
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web4}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                alt=""
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web5}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                alt=""
+              />
+            </div>
+          </div>
         </section>
       </main>
     </div>
